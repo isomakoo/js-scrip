0448fd062d8148c38bca89b4fc119beeba288300
isomakoo <161478022+isomakoo@users.noreply.github.com>
2024-04-02 19:37:01 +0500

    index.js

---
+++ index.js
@@ -0,0 +1,79 @@
+ // let yoshi=prompt("yoshini kiriting")
+// yoshi>20? console.log("hush kelibsz"):console.log("yoshingiz mos kelmadi")
+// 2
+// let a=prompt("a tomonini kiriting")
+// let b=prompt("b tomonini kiriting")
+// let c=prompt("c tomonini kiriting")
+// if(a>b, a>c, b+c>a){
+//     console.log("uchburchak hosil qilinadi")
+// }
+// if(c>b, c>a, b+a>c){
+//     console.log("uchburchak hosil qilinadi")
+// }
+// if(b>a, b>c, a+c>b){
+//     console.log("uchburchak hosil qilinadi")
+// }
+// else{ console.log("uchburchak hosil qilinmaydi")}
+// 3-misol
+// let  a=Number(prompt("a sonini kiriting"))
+// let  b=Number(prompt("b sonini kiriting"))
+// console.log("a*b=", a*b, "a va b sonni kupaytmasi" )
+// console.log("a+b=", a+b, "a va b sonni yigindisi")
+// 4-misol
+// let a= Number(prompt(" a sonnikiriting"))
+// if(a>0){
+//     console.log(Math.sqrt(a), "a soni musbat ")
+// }
+// else{
+//     console.log(a, "son manfiy")
+// }
+// misol-5
+// let b=Number(prompt(" b sonni kiriting"))
+// if(Math.sqrt(b)>0){
+//     console.log("sonn musbat")
+// }
+// else{
+//     console.log("manfiy son")
+// }
+// 6-misol
+// let kabisa=Number(prompt("kabisa yilini kiriting"))
+// if(kabisa%4==0){
+//     console.log("kabisa yili")
+// }
+// else{
+//     console.log("kabisa yili emas")
+// }
+// 7-misol
+// let b=Number(prompt(" b sonni kiriting"))
+// if(b%3==0){
+//     console.log("Fizz")
+// }
+// else if(b%5==0){
+//     console.log("Buzz")
+// }
+// else if(b%3==0, b%5==0){
+//     console.log("FizzBuzz")
+// }
+// 8-misol
+// let  b=Number(prompt(" b sonni kiriting"))
+// let  a=Number(prompt(" a sonni kiriting"))
+// if(b%a==0){
+//     console.log("sonlar bulinadi")
+// }
+//  else console.log("sonlar bulinmaydi")
+// 9-misol
+// let  b=Number(prompt(" b sonni kiriting"))-0
+// let  a=Number(prompt(" a sonni kiriting"))-0
+// let  c=Number(prompt(" c sonni kiriting"))-0
+// if((a+b+c)%3==0){
+//     console.log( Math.exp(a+b+c),)
+// }
+// else {
+//     console.log(Math.floor((a+b+c)/3))
+// }
+// 10-misol
+// let a=Number(prompt("a sonni kiriting"))
+// if(a%2==0){
+//     console.log("juft son")
+// }
+// else console.log("toq son")
